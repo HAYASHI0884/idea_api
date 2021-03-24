1a8a69d8f5ae73788407c479e91f6fa425c2ade1
林将大 <lin.jiangda884@gmail.com>
2021-03-24 18:47:29 +0900

    マイグレーションファイルの編集

--- db/migrate/20210324084928_create_ideas.rb
+++ db/migrate/20210324084928_create_ideas.rb
@@ -1,7 +1,8 @@
 class CreateIdeas < ActiveRecord::Migration[6.0]
   def change
     create_table :ideas do |t|
-
+      t.text       :body,     null: false
+      t.references :category, null: false
       t.timestamps
     end
   end
